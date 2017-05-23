3b978e74d3fd1799c126995ac9160be35ca6ccb3
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-23 13:07:11 -0700

    cleaned up completed game

--- game.py
+++ game.py
@@ -2,15 +2,11 @@
 
 import random
 
-# Put your code here
 user_name = raw_input("Welcome! What should I call you? ")
 print "Hey %s!" % (user_name)
 
 random_num = random.randint(0, 100)
-#user_num = int(raw_input("Guess a number between 0 and 100! "))
 num_guesses = 0
-
-#print random_num
 
 
 while True:
@@ -21,7 +17,6 @@ while True:
         else:
             print "Your guess is too high! Try again! "
         num_guesses += 1
-        #print num_guesses
     else:
         print "Congratulations, %s! You got it right in %d guesses! " % (user_name, num_guesses)
         break

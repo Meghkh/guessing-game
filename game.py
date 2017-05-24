9398e05214d13a195f20b9a455c71c4cda4a9693
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-23 18:00:24 -0700

    ROUNDING WORKSpython game.py python game.py python game.py python game.py python game.py

--- game.py
+++ game.py
@@ -1,5 +1,4 @@
 """A number-guessing game."""
-
 import random
 
 user_name = raw_input("Welcome! What should I call you? ")
@@ -10,16 +9,24 @@ num_guesses = 1
 
 
 while True:
+    user_num = raw_input("Guess a number between 0 and 100! ")
     try:
-        user_num = int(raw_input("Guess a number between 0 and 100! "))
-        if user_num != random_num:
-            if user_num < random_num:
-                print "Your guess is too low! Try again! "
-            else:
-                print "Your guess is too high! Try again! "
-            num_guesses += 1
-        else:
-            print "Congratulations, %s! You got it right in %d guesses! " % (user_name, num_guesses)
-            break
+        user_int = int(user_num)
     except ValueError:
-        print "Oops! Invalid number! Try again! "
+        #print "Oops! Invalid number! Try again! "
+        try:
+            user_float = float(user_num)
+            user_int = round(user_float)
+            print "Float recognized. We will round for you. Guess is %d" % (user_int)
+        except ValueError:
+            print "Oops! Invalid number! Try again! "
+
+    if user_int != random_num:
+        if user_int < random_num:
+            print "Your guess is too low! Try again! "
+        else:
+            print "Your guess is too high! Try again! "
+        num_guesses += 1
+    else:
+        print "Congratulations, %s! You got it right in %d guesses! " % (user_name, num_guesses)
+        break
